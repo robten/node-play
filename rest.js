bbbd1262daeee8aa17f6a4c23bee49b4bed8a0d9
robten <robert.seemel@gmail.com>
2014-01-11 20:42:32 +0100

    Refactored main function.

--- rest.js
+++ rest.js
@@ -1,3 +1,6 @@
+// A simple server that exposes a list of strings through a RESTful API
+// For testing, query it with curl using valide POST, GET, PUT and DELETE requests.
+
 var http = require('http');
 var parse = require('url').parse;
 var items = [];
@@ -11,66 +14,90 @@ function getIndex(req) {
     return index;
 }
 
+function create(req, res) {
+    var item = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+        item += chunk;
+    });
+    req.on('end', function() {
+        items.push(item);
+        console.log('data:', item);
+        res.writeHead(200);
+        res.end('ok\n');
+    });
+}
+
+function read(res) {
+    var body = items.map(function(item, i) {
+        return i + '.) ' + item + '.';
+    }).join('\n');
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset="utf-8"',
+        'Content-Length': Buffer.byteLength(body)
+    });
+    res.end(body);
+}
+
+function update(req, res) {
+    index = getIndex(req);
+    if (index === false) {
+        res.writeHead(400);
+        res.end('No valid item index given.');
+    } else if (!items[index]) {
+        res.writeHead(404);
+        res.end('No item with that index.');
+    } else {
+        var data = '';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk) {
+            data += chunk;
+        });
+        req.on('end', function() {
+            items[index] = data;
+            res.writeHead(200);
+            res.end('Item updated.');
+        });
+    }
+}
+
+function del(req, res) {
+    index = getIndex(req);
+    if (index === false) {
+        res.writeHead(400);
+        res.end('No valid item index given.');
+    } else if (!items[index]) {
+        res.writeHead(404);
+        res.end('No item with that index.');
+    } else {
+        items.splice(index, 1);
+        res.writeHead(200);
+        res.end('Item deleted.');
+    }
+}
+
+function error(res) {
+    res.writeHead(500);
+    res.end('Internal server error');
+}
+
 function main(req, res) {
     var index;
     switch (req.method) {
-        case 'POST': // ==> create
-            var item = '';
-            req.setEncoding('utf8');
-            req.on('data', function(chunk) {
-                item += chunk;
-            });
-            req.on('end', function() {
-                items.push(item);
-                console.log('data:', item);
-                res.writeHead(200);
-                res.end('ok\n');
-            });
+        case 'POST':
+            create(req, res);
             break;
-        case 'PUT': // ==> update
-            index = getIndex(req);
-            if (index === false) {
-                res.writeHead(400);
-                res.end('No valid item index given.');
-            } else if (!items[index]) {
-                res.writeHead(404);
-                res.end('No item with that index.');
-            } else {
-                var data = '';
-                req.setEncoding('utf8');
-                req.on('data', function(chunk) {
-                    data += chunk;
-                });
-                req.on('end', function() {
-                    items[index] = data;
-                    res.writeHead(200);
-                    res.end('Item updated.');
-                });
-            }
+        case 'GET':
+            read(res);
             break;
-        case 'DELETE': // ==> delete
-            index = getIndex(req);
-            if (index === false) {
-                res.writeHead(400);
-                res.end('No valid item index given.');
-            } else if (!items[index]) {
-                res.writeHead(404);
-                res.end('No item with that index.');
-            } else {
-                items.splice(index, 1);
-                res.writeHead(200);
-                res.end('Item deleted.');
-            }
+        case 'PUT':
+            update(req, res);
             break;
-        default: // GET ==> read
-            var body = items.map(function(item, i) {
-                return i + '.) ' + item + '.';
-            }).join('\n');
-            res.writeHead(200, {
-                'Content-Type': 'text/plain; charset="utf-8"',
-                'Content-Length': Buffer.byteLength(body)
-            });
-            res.end(body);
+        case 'DELETE':
+            del(req, res);
+            break;
+        default:
+            error(res);
     }
 }
 
